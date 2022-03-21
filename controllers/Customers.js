f7151b2fd8a1e542be1a0fb812185fd5ca02dd2c
Betoml5 <betoml5@hotmail.com>
2022-03-21 11:58:28 -0600

    Add findByName controler

--- controllers/Customers.js
+++ controllers/Customers.js
@@ -1,16 +1,28 @@
 const User = require("../models/User");
-const responseHTTP = require("../network/response")
+const responseHTTP = require("../network/response");
 
 const controller = {
-    find: async (req, res) => {
-        try {
-            const users = await User.find({}).select("-password");
-            const customers = users.filter(usuario => usuario.role !== "admin");
-            return responseHTTP.success(req, res, customers, 200)
-        } catch (error) {
-            return responseHTTP.error(req, res, error, 500)
-        }
+  find: async (req, res) => {
+    try {
+      const users = await User.find({}).select("-password");
+      const customers = users.filter((usuario) => usuario.role !== "admin");
+      return responseHTTP.success(req, res, customers, 200);
+    } catch (error) {
+      return responseHTTP.error(req, res, error, 500);
     }
-}
+  },
+  findByName: async (req, res) => {
+    const { name } = req.query;
+    try {
+      const customers = await User.find({
+        firstName: { $regex: ".*" + name + ".*" },
+      });
+
+      return responseHTTP.success(req, res, customers, 200);
+    } catch (error) {
+      return responseHTTP.error(req, res, error, 500);
+    }
+  },
+};
 
 module.exports = controller;
--- routes/Customers.js
+++ routes/Customers.js
@@ -1,10 +1,18 @@
 const router = require("express").Router();
 const controller = require("../controllers/Customers");
-const middlewares = require("../middlewares/index")
+const middlewares = require("../middlewares/index");
 
-
-
-router.get("/all", middlewares.verifyAuth, middlewares.verifyRole, controller.find)
-
+router.get(
+  "/all",
+  middlewares.verifyAuth,
+  middlewares.verifyRole,
+  controller.find
+);
+router.get(
+  "/customer",
+  middlewares.verifyAuth,
+  middlewares.verifyRole,
+  controller.findByName
+);
 
 module.exports = router;
